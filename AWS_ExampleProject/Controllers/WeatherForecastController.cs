291f3144ce35871c0a83b10cd24ccc9bf9e1855c
MamunAG <52345044+MamunAG@users.noreply.github.com>
2024-03-01 14:31:19 +0600

    Update WeatherForecastController.cs

--- AWS_ExampleProject/Controllers/WeatherForecastController.cs
+++ AWS_ExampleProject/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace AWS_ExampleProject.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing11", "Bracing11", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
