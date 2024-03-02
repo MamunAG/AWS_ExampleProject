45333249b23612a4976ba4dc10dfda601f748470
MamunOfficeLaptop <mamun166036@gmail.com>
2024-03-02 10:51:57 +0600

    final text for cicd

--- AWS_ExampleProject/Controllers/WeatherForecastController.cs
+++ AWS_ExampleProject/Controllers/WeatherForecastController.cs
@@ -26,7 +26,7 @@ namespace AWS_ExampleProject.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)] + " <this is by cicd pipeline>"
             })
             .ToArray();
         }
